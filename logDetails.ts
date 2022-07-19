ed557329080041288b6693f94c5c27e18efdd435
diogo.cerutti <diogo.cerutti19@gmail.com>
2022-07-19 17:52:29 -0500

    type and Alias

---
+++ logDetails.ts
@@ -0,0 +1,25 @@
+// type Alias
+// | = "pipe"
+type Uid = number | string | undefined;
+
+function logDetails(uid: Uid, item: string) {
+  console.log(`A product with ${uid} has a title as ${item}.`);
+}
+
+function logInfo(uid: Uid, user: string) {
+  console.log(`A user with ${uid} has a name as ${user}.`);
+}
+
+type Platform = "Windows" | "Linux" | "Mac Os" | "Ios";
+
+function renderPlatform(platform: Platform) {
+  return platform;
+}
+
+renderPlatform("Ios");
+
+logDetails(123, "sapato");
+logDetails("123", "sapato");
+
+logInfo(123, "Willian");
+logInfo("123", "Willian");
--- types.ts
+++ types.ts
@@ -1,23 +1,23 @@
 // TIPOS DE DADOS
 
 // boolean (true/false)
-let isOpen: boolean
-isOpen = true
+let isOpen: boolean;
+isOpen = true;
 
 // string ('foo', "foo", 'foo')
-let message: string
-message = `foo ${isOpen}`
+let message: string;
+message = `foo ${isOpen}`;
 
 // number (int, float, hex, binary)
-let total: number
-total = 0xff0
+let total: number;
+total = 0xff0;
 
 // array (type[])
-let items: string[]
-items = ["foo", "bar"]
+let items: string[];
+items = ["foo", "bar"];
 
-let values: Array<number>
-values = [1, 2, 3]
+let values: Array<number>;
+values = [1, 2, 3];
 
 // tuple
 let title: [number, string, string];
@@ -25,8 +25,8 @@ title = [1, "foo", "bar"];
 
 // enum
 enum Colors {
-    white = "#fff",
-    black = "#000",
+  white = "#fff",
+  black = "#000",
 }
 
 // any (qualquer coisa) NÃO É LEGAL!
@@ -35,21 +35,37 @@ coisa = [1];
 
 // void (vazio)
 function logger(): void {
-    console.log('foo')
+  console.log("foo");
 }
 
 // null | undefined
-let variable: null // não recomendado
-type Bla = string | undefined
+let variable: null; // não recomendado
+type Bla = string | undefined;
 
 // never
-function error(): never { // não tem retorno nenhum
-    throw new Error("error");
+function error(): never {
+  // não tem retorno nenhum
+  throw new Error("error");
 }
 
 // object (tudo aquilo que não é string, booleano ou número)
-let cart: object
+let cart: object;
 
 cart = {
-    key: "fi"
-}
+  key: "fi",
+};
+
+// Type Inference
+let message2 = "mensagem definida";
+message2 = "string nova";
+
+// let message2: string = "mensagem definida" -> Redundância
+
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+window.addEventListener("click", (e: MouseEvent) => {
+  // Redundância
+  console.log(e.target);
+});
